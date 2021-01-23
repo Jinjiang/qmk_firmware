29ad9ba40a7f93fdd652e7cd2cc3b1fdcf724449
Jinjiang <zhaojinjiang@me.com>
2021-01-24 01:44:58 +0800

    Update keycap.c v2

--- keyboards/dg/redox/keymaps/default/keymap.c
+++ keyboards/dg/redox/keymaps/default/keymap.c
@@ -6,13 +6,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌─────────┬─────────┬─────────┬─────────┬─────────┬─────────┐                                              ┌─────────┬─────────┬─────────┬─────────┬─────────┬─────────┐
       KC_ESC  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                                                KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSPC ,
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┐                          ┌─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
-      KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_MINS ,                            KC_EQL  , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSLS ,
+      KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_GRV  ,                            KC_QUOT , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSLS ,
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤                          ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
-      KC_GRV  , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_LBRC ,                            KC_RBRC , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT ,
+      KC_CAPS , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , XXXXXXX ,                            XXXXXXX , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_ENT  ,
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┐      ┌─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
-      KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_PGUP , KC_PGDN ,        KC_HOME , KC_END  , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT ,
+      KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_LBRC , KC_RBRC ,        KC_MINS , KC_EQL  , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT ,
   //├─────────┼─────────┼─────────┼─────────┼────┬────┴────┬────┼─────────┼─────────┤      ├─────────┼─────────┼───┬─────┴───┬─────┼─────────┼─────────┼─────────┼─────────┤
-      _______ , KC_LCTL , KC_LALT , KC_LGUI ,      KC_SPC  ,      MO(1)   , MO(2)   ,        MO(2)   , MO(1)   ,     KC_ENT  ,       KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT
+      KC_LCTL , XXXXXXX , KC_LCTL , KC_LALT ,      KC_LGUI ,      KC_SPC  , MO(1)   ,        MO(2)   , KC_ENT  ,     KC_RGUI ,       KC_LEFT , KC_UP   , KC_DOWN , KC_RGHT
   //└─────────┴─────────┴─────────┴─────────┘    └─────────┘    └─────────┴─────────┘      └─────────┴─────────┘   └─────────┘     └─────────┴─────────┴─────────┴─────────┘
   ),
 
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┐                          ┌─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
       _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_F11  ,                            KC_F12  , XXXXXXX , XXXXXXX , KC_UP   , XXXXXXX , XXXXXXX , XXXXXXX ,
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤                          ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
-      KC_CAPS , XXXXXXX , XXXXXXX , XXXXXXX , KC_F    , XXXXXXX , XXXXXXX ,                            XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN , KC_RGHT , XXXXXXX , XXXXXXX ,
+      KC_CAPS , XXXXXXX , XXXXXXX , XXXXXXX , KC_F    , XXXXXXX , XXXXXXX ,                            XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN , KC_RGHT , XXXXXXX , _______ ,
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┐      ┌─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
       _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ ,
   //├─────────┼─────────┼─────────┼─────────┼────┬────┴────┬────┼─────────┼─────────┤      ├─────────┼─────────┼───┬─────┴───┬─────┼─────────┼─────────┼─────────┼─────────┤
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┐                          ┌─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
       XXXXXXX , RGB_M_P , RGB_M_B , RGB_M_R , RGB_M_SW, XXXXXXX , XXXXXXX ,                            KC_MPRV , RGB_MOD , RGB_HUI , RGB_SAI , RGB_VAI , XXXXXXX , XXXXXXX ,
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤                          ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
-      XXXXXXX , RGB_M_SN, RGB_M_X , RGB_M_G , RGB_M_T , RGB_M_K , XXXXXXX ,                            XXXXXXX , RGB_RMOD, RGB_HUD , RGB_SAD , RGB_VAD , XXXXXXX , XXXXXXX ,
+      XXXXXXX , RGB_M_SN, RGB_M_X , RGB_M_G , RGB_M_T , RGB_M_K , XXXXXXX ,                            XXXXXXX , RGB_RMOD, RGB_HUD , RGB_SAD , RGB_VAD , XXXXXXX , _______ ,
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┐      ┌─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
       _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ ,
   //├─────────┼─────────┼─────────┼─────────┼────┬────┴────┬────┼─────────┼─────────┤      ├─────────┼─────────┼───┬─────┴───┬─────┼─────────┼─────────┼─────────┼─────────┤
-      RGB_TOG , _______ , _______ , _______ ,      _______ ,      _______ , _______ ,        _______ , _______ ,     _______ ,       _______ , _______ , _______ , _______ 
+      _______ , RGB_TOG , _______ , _______ ,      _______ ,      _______ , _______ ,        _______ , _______ ,     _______ ,       _______ , _______ , _______ , _______ 
   //└─────────┴─────────┴─────────┴─────────┘    └─────────┘    └─────────┴─────────┘      └─────────┴─────────┘   └─────────┘     └─────────┴─────────┴─────────┴─────────┘
   )
 
